4913f49dd968c577ef0f413c332ffc67673fb05c
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-11 17:28:04 +0000

    Create ball-class.py

---
+++ ball-class.py
@@ -0,0 +1,14 @@
+class Ball: 
+  def __init__(self,type,colour,diameter): 
+      self.type = type
+      self.colour = colour 
+      self.diameter = diameter 
+def MyBall(self): 
+   print('Type:  '+ self.type) 
+   print('Colour: '+ self.colour) 
+   print('Diameter: '+ self.diameter) 
+  
+p1 = Ball('Netball','Pink','2.3cm') 
+
+p1.MyBall() 
+
