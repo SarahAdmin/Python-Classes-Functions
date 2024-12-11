430b74d8157f5cceda2b6f445919c587be070ee9
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-11 18:07:58 +0000

    Create Max-Numbers.py

---
+++ Max-Numbers.py
@@ -0,0 +1,11 @@
+def MaximumNumbers(numbers): 
+  numbers[0]
+  numbers[1]
+  numbers[2]
+  numbers[3]
+  numbers[4]
+  numbers[5]
+  return max(...numbers) 
+
+print(MaximumNumbers([34,26,328,93,46,73]))
+
