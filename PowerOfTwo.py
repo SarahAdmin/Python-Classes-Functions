ec65b727969bd446c708b4d36da737e5030ad95f
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-11 14:28:00 +0000

    Create PowerOfTwo.py

---
+++ PowerOfTwo.py
@@ -0,0 +1,9 @@
+def PoweredNumbers(num): 
+  return pow(num,2) 
+print(PoweredNumbers(2))
+print(PoweredNumbers(3))
+print(PoweredNumbers(4))
+print(PoweredNumbers(8))
+print(PoweredNumbers(16)) 
+print(PoweredNumbers(128))
+
