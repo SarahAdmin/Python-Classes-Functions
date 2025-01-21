86c39343142b845ba5063e91f2f31ed2d7a91498
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-01-21 11:21:19 +0000

    Create FindAverageNumber.py

---
+++ FindAverageNumber.py
@@ -0,0 +1,13 @@
+import statistics
+def FindAverage(num): 
+  num[0]
+  num[1]
+  num[2]
+  num[3]
+  num[4]
+  num[5] 
+  return statistics.mean(num)
+
+print(FindAverage([38,25,93,48,29,39]))
+
+  
