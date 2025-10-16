33f1ada76f00a6af76827fb58fc9b97f34ab8594
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-10-16 21:26:23 +0100

    Fix data structure for car data input

--- car_project.py
+++ car_project.py
@@ -2,7 +2,7 @@ import pandas as pd
 def myCars(input_data,column_names): 
   df = pd.DataFrame(input_data,columns=column_names)
   return df 
-data = [["Toyota","Ford","Volkswagen"],["Yaris,","Fiesta","T-Cross"],:["Red","Blue","Black"],:[5,5,5],[2019,2024,2020]]
+data = [["Toyota","Yaris","Red",5,2019],["Ford","Fiesta","Blue",5,2024],["Volkswagen","T-Cross","Black",5,2020]]
 cols = ['Make','Model','Colour','No_of_Seats','Year']
 final_data = myCars(data,cols)
 print(final_data)
