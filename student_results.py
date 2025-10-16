4f243d3133186c0973eb1eeebced5c0136fc6b50
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-10-16 19:18:06 +0100

    Add CollegeResults function to process student data

---
+++ student_results.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+def CollegeResults(input_data,column_names): 
+  df = pd.DataFrame(input_data,columns=column_names)
+  return df 
+
+myData = [['Lee Cox','Matt Bond','Lemon Jelly','Marcellina Adams','Jill Gates','Joe Malone'],
+         ['FS','FS','FS','FS','FS','FS'], 
+         ['L1','L1','E3','E3','L1','E3'],
+         ['English','Maths','English','Maths','English','Maths'],
+         [70,100,65,30,40,80],
+         [True,True,True,False,False,True],
+         [False,False,False,True,True,False]]
+cols = ['Name','Qualification','Level','Subject','Percentage','Passed','Retake?']
+final_data = CollegeResults(myData,cols)
+print(final_data)
