041c9a0100b931318921d9a497ba0f7f457ede76
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-10-16 21:31:03 +0100

    Refactor student data structure for CollegeResults

--- student_results.py
+++ student_results.py
@@ -3,11 +3,10 @@ def CollegeResults(input_data,column_names):
   df = pd.DataFrame(input_data,columns=column_names)
   return df 
 
-myData = [['Lee Cox','Matt Bond','Lemon Jelly','Marcellina Adams','Jill Gates','Joe Malone'],
-         ['FS','FS','FS','FS','FS','FS'], 
-         ['L1','L1','E3','E3','L1','E3'],
-         ['English','Maths','English','Maths','English','Maths'],
-         [70,100,65,30,40,80]]
+myData = [['Lee Cox','FS','L1','English',70],['Matt Bond','FS','L1','Maths',100],['Lemon Jelly','FS','E3','English',65],['Marcellina Adams','FS''E3','Maths',30],
+ ['Jill Gates','FS','L1','English',40],['Joe Malone','FS','E3','Maths',80]]
 cols = ['Name','Qualification','Level','Subject','Percentage']
 final_data = CollegeResults(myData,cols)
 print(final_data)
+
+
